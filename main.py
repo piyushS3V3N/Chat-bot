f91fcbf25109a12e03fe32f6a390bb9c2a343a89
piyushS3V3N <45173742+piyushS3V3N@users.noreply.github.com>
2020-09-05 17:36:35 +0530

    added new feature and fixed accuracy issues
    increased epoch making more data training accessible

--- main.py
+++ main.py
@@ -1,28 +1,25 @@
 #! /usr/bin/python
 import nltk
 from nltk.stem import WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
 import pickle
 import numpy as np
-import json
-import random
 
 from keras.models import load_model
 
-lemmatizer = WordNetLemmatizer()
+model= load_model('chatbot_model.h5')
 
-model = load_model('chatbot_model.h5')
-
+import json
+import random
 
 intents = json.loads(open('intents.json').read())
 words = pickle.load(open('words.pkl','rb'))
 classes = pickle.load(open('classes.pkl','rb'))
 
-
 def clean_up_sentence(sentence):
     sentence_words = nltk.word_tokenize(sentence)
     sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
     return sentence_words
-
 
 def bow(sentence, words, show_details=True):
 
@@ -30,58 +27,50 @@ def bow(sentence, words, show_details=True):
 
     bag = [0]*len(words)
     for s in sentence_words:
-        for i, w in enumerate(words):
+        for i,w in enumerate(words):
             if w == s:
                 bag[i] = 1
                 if show_details:
                     print("found in bag: %s" % w)
     return(np.array(bag))
 
-
 def predict_class(sentence, model):
     p = bow(sentence, words, show_details=False)
     res = model.predict(np.array([p]))[0]
-    ERROR_THRESHOLD = 0.25
-    results = [[i, r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
+    ERROR_THRESHOLD= 0.25
+    results = [[i,r] for i,r in enumerate(res) if r>ERROR_THRESHOLD]
     results.sort(key=lambda x: x[1], reverse=True)
     return_list = []
     for r in results:
-        return_list.append({"intent": classes[r[0]], "probability": str(r[1])})
+        return_list.append({"intent":classes[r[0]], "probability" : str(r[1])})
     return return_list
-
-
 def getResponse(ints, intents_json):
     tag = ints[0]['intent']
     list_of_intents = intents_json['intents']
     for i in list_of_intents:
-        if i['tag']== tag:
+        if(i['tag']== tag):
             result = random.choice(i['responses'])
             break
     return result
-
 
 def chatbot_response(msg):
     ints = predict_class(msg, model)
     res = getResponse(ints, intents)
     return res
 
+def send():
+    #if msg != '':
+    res= chatbot_response(msg)
+    print("[BOT]: " + res)
+    #else:
+    #    res = chatbot_response(msg)
 
-def send(msg):
-    res = chatbot_response(msg)
-    return res
+while True:
+    msg = input("[YOU]: ")
+    if msg != 'bye':
+        send()
+    else:
+        send()
+        break
 
-
-def start(user_input):
-    res = send(user_input)
-    return res
-
-
-# while True:
-#     msg = input("[YOU]: ")
-#     if msg != 'bye':
-#         send()
-#     else:
-#         send()
-#         break
-
-# chatbot_response('hello')
+#chatbot_response('hello')
--- nltkbot.py
+++ nltkbot.py
@@ -77,11 +77,11 @@ model.add(Dropout(0.5))
 model.add(Dense(len(train_y[0]), activation='softmax'))
 
 # Compile model. Stochastic gradient descent with Nesterov accelerated gradient gives good results for this model
-sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+sgd = SGD(lr=0.01, decay=1e-6, momentum=0.8, nesterov=True)
 model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
 
 #fitting and saving the model
-hist = model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
+hist = model.fit(np.array(train_x), np.array(train_y), epochs=500, batch_size=10, verbose=1)
 model.save('chatbot_model.h5', hist)
 
-print("model created")
+print("\033[1;32;40m [INFO] \033[00m : model created")
