663bd7f8d6c7776e9a9eef71e777dd5ab8a18be6
Sharad-Jain-24 <57042640+Sharad-Jain-24@users.noreply.github.com>
2020-11-03 22:06:57 +0530

    added user info in mail in handleapi

--- handle-api.py
+++ handle-api.py
@@ -52,23 +52,30 @@ def emailHandle():
     EMAIL_CONTENT = ""
 
     email_to = {
-        1.1: "piyushparashar2k@gmail.com",
-        2.1: "piyushparashar2k@gmail.com",
-        2.2: "",
+        # 1.1: "",
+        2.1: "studentsupport@tips.edu.in",
+        2.2: "studentsupport@tips.edu.in",
         2.3: "",
-        3.3: ""
+        3.3: "accounts@tips.edu.in"
     }
 
-    if case == 1.1:
-        EMAIL_CONTENT = ""
-    elif case == 2.1:
-        EMAIL_CONTENT = "Degree Query for course: " + req_data["course"] + ". Batch: " + req_data["batch"] + ". Drop out case: " + req_data["drop"]
+    # name, roll, mail
+    user_info = "Name - " + req_data["name"] + ", Enrollment number - " + req_data["roll"] + ", E-mail ID - " + \
+                req_data["mail"] + ", "
+
+    # if case == 1.1:
+    #     EMAIL_CONTENT = ""
+    if case == 2.1:
+        EMAIL_CONTENT = "Degree Query for course: " + req_data["course"] + ". Batch: " + req_data[
+            "batch"] + ". Drop out case: " + req_data["drop"]
     elif case == 2.2:
         EMAIL_CONTENT = "Bonafide for purpose: " + req_data["purpose"]
     elif case == 2.3:
         EMAIL_CONTENT = ""
     elif case == 3.3:
         EMAIL_CONTENT = "Accounts Query for security for batch: " + req_data["batch"] + ". Year: " + req_data["year"]
+
+    EMAIL_CONTENT = user_info + EMAIL_CONTENT
 
     service = mail.service_account_login()
     message = mail.create_message(EMAIL_FROM, email_to.get(case), EMAIL_SUBJECT, EMAIL_CONTENT)
